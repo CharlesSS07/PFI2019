679d6b9091db22b4ec354f139567450cf9f076e9
CharlesSS07 <charles.s.strauss@gmail.com>
2019-03-28 16:01:34 -0600

    excluded params files, and jupyter notbooks checkpoints from git

--- run_params.sh
+++ run_params.sh
@@ -1,5 +1,5 @@
 source activate tensorflowgpu
-python3 train.py --params 'params_1.yaml'
-python3 train.py --params 'params_2.yaml'
-python3 train.py --params 'params_3.yaml'
-python3 train.py --params 'params_4.yaml'
+python3 train.py --params 'params/params_1.yaml'
+python3 train.py --params 'params/params_2.yaml'
+python3 train.py --params 'params/params_3.yaml'
+python3 train.py --params 'params/params_4.yaml'
