ca7480d823cd8270c37a3ffd62f28bdcfdc97a33
CharlesSS07 <charles.s.strauss@gmail.com>
2019-03-30 10:00:32 -0600

    Updated sstt notebook.

--- run_params.sh
+++ run_params.sh
@@ -1,5 +1,3 @@
 source activate tensorflowgpu
-python3 train.py --params 'params/params_1.yaml'
-python3 train.py --params 'params/params_2.yaml'
-python3 train.py --params 'params/params_3.yaml'
-python3 train.py --params 'params/params_4.yaml'
+python3 model.py --params 'params/params_1.yaml'
+python3 model.py --params 'params/params_2.yaml'
