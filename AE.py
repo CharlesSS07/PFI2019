Fixed error with shape of array when unnormalized. Array was returned with extra dimension of size 1, which must me squashed. Normalization would usuially squash automatically, however when unnormalized causes error. Now Ae returns latent layer in correct shape.

AE.py
@@ -355,7 +355,7 @@ class AE():
         for p in self.PPI.data:
             self.training[self.X] = [p]
             c.append(self.sess.run(self.latent, feed_dict=self.training))
-        return np.asarray(c)
+        return np.asarray(c)[:,0]
     
     def save(self):
         '''

model.py
@@ -29,7 +29,7 @@ def run(paramsfile='None', show=False):
         ae.save()
         c = ae.compress()
         if ph.get('postnormalization', False):
-            c = (c[:,0]-c[:,0].mean(axis=0))/np.std(c[:,0], axis=0)
+            c = (c-c.mean(axis=0))/np.std(c, axis=0)
         ae.pca_summary(c, 'latent')
         knn = KNN(pm, ph, go, c)
     elif do_pca: